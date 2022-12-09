d93bd0ce66ca2d5064d8675f44c537622d4c007e
Ansha Prashanth <anshaprashanth@gmail.com>
2022-12-09 22:04:55 +0530

    revised

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/blog-app",
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
+import Auth from "./components/Auth";
+import { authActions } from "./store";
+import { useDispatch, useSelector } from "react-redux";
+
+
+function App() {
+
+  const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      dispatch(authActions.login());
+    }
+  }, [dispatch]);
+
+  return (
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          {!isLoggedIn ? (
+            <Route path="/auth" element={<Auth />} />
+          ) : (
+            <>
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/blogs/add" element={<AddBlog />} />
+              <Route path="/myBlogs" element={<UserBlogs />} />
+              <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+            </>
+          )}
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
